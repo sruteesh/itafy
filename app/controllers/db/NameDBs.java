2722b207714e16f2e6a7feacf1b747ab6b49c69b
Raúl Marcos <raulmarcosl@gmail.com>
2014-03-01 04:09:13 +0100

    Added benchmarks' mongo names

--- app/controllers/db/NameDBs.java
+++ app/controllers/db/NameDBs.java
@@ -4,6 +4,7 @@ public class NameDBs
 {
 	// DBs names
 	public static final String DB_ITAFY = "itafy";
+	public static final String DB_ITAFY_BENCHMARKS = "itafy-benchmarks";
 
 	// collections names
 	public static final String USERS = "users";
@@ -11,4 +12,6 @@ public class NameDBs
 	public static final String LINKS = "links";
 	public static final String WORDS = "words";
 	public static final String GEO_TWEETS = "geo_tweets";
+
+	public static final String TWITTER_NAMES = "twitter_names";
 }
