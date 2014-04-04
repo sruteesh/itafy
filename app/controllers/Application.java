773496dc865379f0bf5b54078669aab027c0d0d8
manutero <manutero@gmail.com>
2014-04-04 10:43:50 +0200

    JavaScript router example working. (calling javaScript from java)
    Note that these files will be removed, but interesting to keep them in one commit at least

---
+++ app/controllers/Application.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import play.Routes;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.ajax_result;
+import views.html.index;
+
+// http://franzgranlund.wordpress.com/2012/03/29/play-framework-2-0-javascriptrouter-in-java/
+public class Application extends Controller {
+
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	public static Result plus(String num1, String num2) {
+		Integer answer = Integer.valueOf(num1) + Integer.valueOf(num2);
+		return ok(ajax_result.render(answer));
+	}
+
+	public static Result minus(String num1, String num2) {
+		Integer answer = Integer.valueOf(num1) - Integer.valueOf(num2);
+		return ok(ajax_result.render(answer));
+	}
+
+	public static Result javascriptRoutes() {
+		response().setContentType("text/javascript");
+		return ok(
+				Routes.javascriptRouter("jsRoutes",
+						// Routes
+						controllers.routes.javascript.Application.plus(),
+						controllers.routes.javascript.Application.minus()
+						)
+				);
+	}
+
+}
