ced3e072e7a0b773f9fdb0e5b3f65a9a791eb8db
manutero <manutero@gmail.com>
2014-04-08 09:29:06 +0200

    Change the database definition as DB_ITAFY instead of DB_ITAFY_BENCHMARKS
    (I can not realize why has it been changed)

--- app/models/data/MongoClientData.java
+++ app/models/data/MongoClientData.java
@@ -1,12 +1,10 @@
 package models.data;
 
 import org.jongo.Jongo;
-
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
-
-import controllers.db.MongoDBHandler;
 import controllers.db.DbNames;
+import controllers.db.MongoDBHandler;
 
 /**
  * Definition of Mongo client and Jongo connection.
@@ -17,7 +15,7 @@ import controllers.db.DbNames;
 public abstract class MongoClientData {
 	protected static final MongoClient mongo = MongoDBHandler.getMongoClient();
 
-	protected static final DB dbItafy = mongo.getDB(DbNames.DB_ITAFY_BENCHMARKS);
+	protected static final DB dbItafy = mongo.getDB(DbNames.DB_ITAFY);
 	protected static final Jongo jongoItafy = new Jongo(dbItafy);
 
 	protected static final DB dbItafyBenchmarks = mongo.getDB(DbNames.DB_ITAFY_BENCHMARKS);
