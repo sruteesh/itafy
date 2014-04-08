a6851660502a44b3a6a10c4f70737ad7e47f03f8
manutero <manutero@gmail.com>
2014-04-08 13:25:21 +0200

    Added functionality "ArrayList<Tweet> getTweets()" to word model; some refactor as code reorganization.

--- app/models/entities/Word.java
+++ app/models/entities/Word.java
@@ -1,60 +1,88 @@
 package models.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
 import models.categories.Category;
+import models.data.TweetsData;
 import org.jongo.marshall.jackson.oid.Id;
 import org.jongo.marshall.jackson.oid.ObjectId;
 import twitter4j.GeoLocation;
+import utils.Helper;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 /**
  * 
  * @author manutero, raulmarcosl
  */
-public class Word
-{
-	private final String text;
-	private final double latitude;
-	private final double longitude;
-	private final Date createdAt;
-	private final Category category;
+public class Word {
 	@Id
-	@ObjectId
-	private String id;
+	@ObjectId private String id;
+
+	private String text;
+	private double latitude;
+	private double longitude;
+	private Category category;
+
+	private Date createdAt;
+	private Date updatedAt;
+
+	@JsonCreator
+	public Word() {
+		this.createdAt = new Date();
+		this.updatedAt = createdAt;
+	}
 
 	private Word(String text, GeoLocation c) {
 		this.text = text;
 		this.latitude = c.getLatitude();
 		this.longitude = c.getLongitude();
-		this.createdAt = new Date();
 		this.category = null;
+		this.createdAt = new Date();
+		this.updatedAt = createdAt;
 	}
 
+	/**
+	 * Factory.
+	 * 
+	 * @param name defines the word itself
+	 * @param geoLocation coordenates
+	 * @return new <code>Word</code> instance
+	 */
 	public static Word createWordWithGeoLocation(String name, GeoLocation geoLocation) {
 		return new Word(name, geoLocation);
 	}
 
-	public String getText() {
-		return text;
+	public void setCategory(Category category) {
+		this.category = category;
+		this.updatedAt = new Date();
 	}
 
-	public double getLatitude() {
-		return latitude;
+	public ArrayList<Tweet> getTweets() {
+		ArrayList<Object> tweets = TweetsData.getTweetsWithWord(this.id);
+		return Helper.castEeachElementToTweet(tweets);
 	}
 
-	public double getLongitude() {
-		return longitude;
-	}
+	@JsonProperty("text")
+	public String getText() { return text; }
 
-	public Date getCreatedAt() {
-		return createdAt;
-	}
+	@JsonProperty("latitude")
+	public double getLatitude() { return latitude; }
 
-	public Category getCategory() {
-		return category;
-	}
+	@JsonProperty("longitude")
+	public double getLongitude() { return longitude; }
 
-	public String getId() {
-		return id;
-	}
+	@JsonProperty("category")
+	public Category getCategory() { return category; }
+
+	@JsonProperty("created_at")
+	public Date getCreatedAt() { return createdAt; }
+
+	@JsonProperty("updated_at")
+	public Date getUpdatedAt() { return updatedAt; }
+
+	@JsonProperty("_id")
+	public String getId() { return id; }
 
 }
