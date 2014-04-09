a59e79927f8cd5de5ed29868793a4ca4172c3860
manutero <manutero@gmail.com>
2014-04-09 14:23:27 +0200

    New file to repository: TweetController (under controllers/api)

---
+++ app/controllers/api/TweetController.java
@@ -0,0 +1,17 @@
+package controllers.api;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+
+public class TweetController extends Controller {
+
+	public static Result index() {
+		return TODO;
+	}
+
+	public static Result show(String id) {
+		return TODO;
+	}
+
+}
