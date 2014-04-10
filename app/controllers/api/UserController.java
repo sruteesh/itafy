2766793f49e21a17aca9e1c71977eb035fcf686a
manutero <manutero@gmail.com>
2014-04-10 11:59:39 +0200

    UserController: doing queries with genre, area and combination of both

--- app/controllers/api/UserController.java
+++ app/controllers/api/UserController.java
@@ -1,7 +1,16 @@
 package controllers.api;
 
+import java.util.ArrayList;
+import models.data.UserData;
+import models.entities.User;
+import models.geoLocation.Area;
+import models.geoLocation.AvaibleLocations;
+import org.codehaus.jackson.JsonNode;
 import play.mvc.Controller;
 import play.mvc.Result;
+import twitter4j.GeoLocation;
+import utils.Helper;
+import utils.ParamsHelper;
 
 
 /**
@@ -13,22 +22,88 @@ import play.mvc.Result;
 public class UserController extends Controller {
 
 	/**
+	 * <pre>
 	 * GET /api/users/
-	 *
-	 * @return (Result) index page
+	 * GET /api/users/? area=a
+	 * GET /api/users/? genre=g
+	 * GET /api/users/? latitude=lat & longitude=lon & radio=r
+	 * GET /api/users/? user=u
+	 * 
+	 * GET /api/users/? area=a & genre=g
+	 * GET /api/users/? latitude=lat & longitude=lon & radio = r & genre=g
+	 * GET /api/users/? area=a & user=u
+	 * GET /api/users/? latitude=lat & longitude=lon & radio=r & user=u
+	 * </pre>
+	 * 
+	 * TODO genre as a enum instead of a String
+	 * FIXME possible multiple parameters: remove the last "get(0)"
 	 */
 	public static Result index() {
-		return redirect("/api/users/help");
+		ParamsHelper params = new ParamsHelper(request().queryString());
+		Area area = defineAreaFromParams(params);
+		String genre = defineGenreFromParams(params);
+		String userName = defineUserFromParams(params);
+		ArrayList<Object> users = executeQueryDependingOn(area, genre, userName);
+		JsonNode response = Helper.asJson(users);
+		return ok(response);
 	}
 
 	/**
 	 * GET /api/users/:id
-	 *
-	 * @param id (String) which user
-	 * @return (Result) JSON format
 	 */
 	public static Result show(String id) {
-		return TODO;
+		User user = UserData.findUser(id);
+		JsonNode response = Helper.asJson(user);
+		return ok(response);
+	}
+
+
+	// private
+
+	private static Area defineAreaFromParams(ParamsHelper params) {
+		Area area = null;
+		if (params.has("area")) {
+			AvaibleLocations trustedLocation = AvaibleLocations.asLocation(params.get("area"));
+			area = Area.createLocation(trustedLocation);
+		} else if ((params.has("latitude")) && (params.has("longitude")) && params.has("radio")) {
+			double latitude = Double.valueOf(params.get("latitude")).doubleValue();
+			double longitude = Double.valueOf(params.get("longitude")).doubleValue();
+			double radio = Double.valueOf(params.get("radio")).doubleValue();
+			GeoLocation coordenates = new GeoLocation(latitude, longitude);
+			area = Area.createLocation(coordenates, radio);
+		}
+		return area;
+	}
+
+	private static String defineGenreFromParams(ParamsHelper params) {
+		String genre = null;
+		if (params.has("genre")) {
+			genre = params.get("genre");
+		}
+		return genre;
+	}
+
+	// TODO : use userName
+	private static ArrayList<Object> executeQueryDependingOn(Area area, String genre, String userName) {
+		ArrayList<Object> users = new ArrayList<Object>();
+		if ((area != null) && (genre != null)) {
+			users = UserData.getUsers(area, genre);
+		} else if (area != null) {
+			users = UserData.getUsers(area);
+		} else if (genre != null) {
+			users = UserData.getUsers(genre);
+		} else {
+			users = UserData.getUsers();
+		}
+		return users;
+	}
+
+	private static String defineUserFromParams(ParamsHelper params) {
+		String userName = null;
+		if (params.has("user")) {
+			userName = params.get("user");
+		}
+		return userName;
 	}
 
 }
