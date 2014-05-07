04433efdf0b09a200a0eb610dd965b0be18b1e9c
manutero <manutero@gmail.com>
2014-05-07 19:47:00 +0200

    Added hits and misses count to TextClassificationBenchmark

--- app/benchmarks/textClassification/TextClassificationBenchmark.java
+++ app/benchmarks/textClassification/TextClassificationBenchmark.java
@@ -2,6 +2,7 @@ package benchmarks.textClassification;
 
 import java.io.File;
 import java.util.HashMap;
+import models.categories.AvaibleCategories;
 import utils.helpers.FileHelper;
 import utils.textClassifier.TextClassifier;
 
@@ -10,10 +11,19 @@ import utils.textClassifier.TextClassifier;
  * @author raul.marcos.l@gmail.com
  */
 public class TextClassificationBenchmark {
+
+	private int hits;
+	private int misses;
+	private int falseNews;
+	private int falseSports;
 	private TextClassifier classifier;
 
 	public TextClassificationBenchmark() {
 		classifier = new TextClassifier();
+		hits = 0;
+		misses = 0;
+		falseNews = 0;
+		falseSports = 0;
 	}
 
 	public void runBenchmarks() {
@@ -23,44 +33,70 @@ public class TextClassificationBenchmark {
 		cultureTest();
 		sportTest();
 		uncategorizedTest();
+		results();
 	}
 
+	private void results() {
+		System.out.println("HITS" + hits);
+		System.out.println("MISS" + misses);
+		System.out.println("NO - NEWS" + falseNews);
+		System.out.println("NO - SPORT" + falseSports);
+	}
 	private void uncategorizedTest() {
 		System.out.println("UNCATEGORIZED\n------------------\n");
 		File uncategorizedFolder = new File(UNCATEGORIZED_TWEETS);
-		test(uncategorizedFolder);
+		test(uncategorizedFolder, "");
 	}
 
 	private void sportTest() {
-		System.out.println("SPORT\n------------------\n");
 		File sportFolder = new File(SPORT_TWEETS);
-		test(sportFolder);
+		test(sportFolder, "DEPORTES");
 	}
 
 	private void cultureTest() {
-		System.out.println("CULTURE\n------------------\n");
 		File cultureFolder = new File(CULTURE_TWEETS);
-		test(cultureFolder);
+		test(cultureFolder, "ACTUALIDAD");
 	}
 
 	private void politicTest() {
-		System.out.println("POLITIC\n------------------\n");
 		File politicFolder = new File(POLITIC_TWEETS);
-		test(politicFolder);
+		test(politicFolder, "ACTUALIDAD");
 	}
 
-	private void test(File folder) {
-		HashMap<String, Double> response = new HashMap<String, Double>();
+	private void test(File folder, String expected) {
 		for (final File fileEntry : folder.listFiles()) {
 			if (FileHelper.isNormalFile(fileEntry)) {
 				String text = FileHelper.readTextFile(fileEntry.getPath());
-				System.out.println(text);
-				response = classifier.classifyMessage(text);
-				System.out.println(response);
-				System.out.println(classifier.getLastClassName());
-				System.out.println("+++++++++++++++++++++++");
+				HashMap<String, Double> response = classifier.classifyMessage(text);
+				String got = classifier.getLastClassName();
+				print(got, expected, response);
+				compare(got, expected);
 			}
 		}
+	}
+
+	private void compare(String got, String expected) {
+		if (mustBeUncategorized(expected)) {
+			if (got.equals(AvaibleCategories.ACTUALIDAD.name())) {
+				this.falseNews++;
+			} else if (got.equals(AvaibleCategories.DEPORTES.name())) {
+				this.falseSports++;
+			}
+		} else {
+			if (got.equals(expected)) {
+				this.hits++;
+			} else {
+				this.misses++;
+			}
+		}
+	}
+
+	private boolean mustBeUncategorized(String s) {
+		return s.isEmpty();
+	}
+
+	private void print(Object got, Object expected, Object details) {
+		System.out.println(got + " =? " + expected + "\t(" + details + ")");
 	}
 
 	private void train(final File folder) {
