dd1d86d9fbc79da8d5ddbaa0073e90ad7e7ec464
manutero <manutero@gmail.com>
2014-05-07 11:35:41 +0200

    Added 3 constants to general MsgClassificationConstants class: POLITICS, CULTURE, SPORTS

--- app/utils/textClassifier/MsgClassificationConstants.java
+++ app/utils/textClassifier/MsgClassificationConstants.java
@@ -4,4 +4,7 @@ public interface MsgClassificationConstants {
 	public static final String DATASET_NAME = "MessageClassification";
 	public static final String MESSAGE = "Message";
 	public static final String CLASS = "Class";
+	public static final String POLITICS = "POLITICA";
+	public static final String CULTURE = "CULTURA";
+	public static final String SPORTS = "DEPORTE";
 }
