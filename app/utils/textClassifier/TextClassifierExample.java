a5587d1895f9003a65c2ace137b042716778242f
manutero <manutero@gmail.com>
2014-05-07 11:29:43 +0200

    Weka's module: fix the TextClassifierExample

--- app/utils/textClassifier/TextClassifierExample.java
+++ app/utils/textClassifier/TextClassifierExample.java
@@ -1,205 +1,157 @@
 package utils.textClassifier;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
 import java.util.HashMap;
 import utils.helpers.FileHelper;
-import weka.core.SerializationHelper;
-import weka.core.Utils;
 
-
+/**
+ * Shows how to use the <code>TextClassifier</code> class
+ * <p>
+ * Contanis three examples
+ * (there is no a super <code>main</code> method, just rename the main you want to execute)
+ * 
+ * @author m.artero@ucm.es
+ * @author raul.marcos.l@gmail.com
+ */
 public class TextClassifierExample {
 
-	// FIXME: relative paths instead of absolute local paths
-	// Stackoverflow#3844307
-
-	private static final String MODEL_PATH =
-			//"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/example-model.arff";
-			"/Users/manutero/workspace/itafy/weka-data/2014_04_29.arff";
-
-	private static final String POLITIC_QUERY_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/query_politica.txt";
-	private static final String ECONOMY_QUERY_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/query_economia.txt";
-	private static final String CULTURE_QUERY_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/query_cultura.txt";
-	private static final String SPORT_QUERY_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/query_deportes.txt";
-
-	private static final String POLITIC_TEXT_1_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/politica_1.txt";
-	private static final String POLITIC_TEXT_2_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/politica_2.txt";
-	private static final String POLITIC_TEXT_3_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/politica_3.txt";
-
-	private static final String ECONOMY_TEXT_1_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/economia_1.txt";
-	private static final String ECONOMY_TEXT_2_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/economia_2.txt";
-	private static final String ECONOMY_TEXT_3_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/economia_3.txt";
-
-	private static final String CULTURE_TEXT_1_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/cultura_1.txt";
-	private static final String CULTURE_TEXT_2_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/cultura_2.txt";
-
-	private static final String SPORT_TEXT_1_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/deportes_1.txt";
-	private static final String SPORT_TEXT_2_PATH =
-			"/Users/manutero/workspace/itafy/app/utils/textClassifier/exampleFiles/deportes_2.txt";
-
+	/**
+	 * First example
+	 * <p>
+	 * Shows how to use the <code>textClassifier</code> class building initially an empty dataset
+	 * and filling it merging multiple <em>arff</em> files
+	 */
 	public static void main(String[] args) {
-
-		TextClassifier classifier = new TextClassifier(MODEL_PATH);
-		System.out.println("Att: " + classifier.getNumAttributes());
-		System.out.println("Classes: " + classifier.getNumClasses());
-		System.out.println("Instances: " + classifier.getNumInstances());
-		System.out.println("----------------------------------");
-
-		HashMap<String, Double> response = new HashMap<String, Double>();
-
-		String cultureQuery = FileHelper.readTextFile(CULTURE_QUERY_PATH);
-		response = classifier.classifyMessage(cultureQuery);
-		System.out.println("We do know it's a cultural text");
-		System.out.println(response);
-
-		String PoliticQuery = FileHelper.readTextFile(POLITIC_QUERY_PATH);
-		response = classifier.classifyMessage(PoliticQuery);
-		System.out.println("We do know it's a politic text");
-		System.out.println(response);
-
-		//		String economyQuery = FileHelper.readTextFile(ECONOMY_QUERY_PATH);
-		//		response = classifier.classifyMessage(economyQuery);
-		//		System.out.println("We do know it's a economic text");
-		//		System.out.println(response);
-
-		String sportQuery = FileHelper.readTextFile(SPORT_QUERY_PATH);
-		response = classifier.classifyMessage(sportQuery);
-		System.out.println("We do know it's a sport text");
-		System.out.println(response);
-
-		System.out.println("END");
-	}
-
-	public static void _main(String[] args) {
-
-		TextClassifierExample example = new TextClassifierExample();
-
-		String[] politica1 = {"-m", POLITIC_TEXT_1_PATH, "-c", "POLITICA", "-t", "example"};
-		example.execute(politica1);
-		String[] politica2 = {"-m", POLITIC_TEXT_2_PATH, "-c", "POLITICA", "-t", "example"};
-		example.execute(politica2);
-		String[] politica3 = {"-m", POLITIC_TEXT_3_PATH, "-c", "POLITICA", "-t", "example"};
-		example.execute(politica3);
-
-		String[] economia1 = {"-m", ECONOMY_TEXT_1_PATH, "-c", "ECONOMIA", "-t", "example"};
-		example.execute(economia1);
-		String[] economia2 = {"-m", ECONOMY_TEXT_2_PATH, "-c", "ECONOMIA", "-t", "example"};
-		example.execute(economia2);
-		String[] economia3 = {"-m", ECONOMY_TEXT_3_PATH, "-c", "ECONOMIA", "-t", "example"};
-		example.execute(economia3);
-
-		String[] cultura1 = {"-m", CULTURE_TEXT_1_PATH, "-c", "CULTURA", "-t", "example"};
-		example.execute(cultura1);
-		String[] cultura2 = {"-m", CULTURE_TEXT_2_PATH, "-c", "CULTURA", "-t", "example"};
-		example.execute(cultura2);
-
-		String[] deportes1 = {"-m", SPORT_TEXT_1_PATH, "-c", "DEPORTES", "-t", "example"};
-		example.execute(deportes1);
-		String[] deportes2 = {"-m", SPORT_TEXT_2_PATH, "-c", "DEPORTES", "-t", "example"};
-		example.execute(deportes2);
-
-
-		String[] queryPolitica = {"-m", POLITIC_QUERY_PATH, "-t", "example"};
-		System.out.println("Query, we do know it's <politica> ");
-		example.execute(queryPolitica);
-
-		String[] queryEconomia = {"-m", ECONOMY_QUERY_PATH, "-t", "example"};
-		System.out.println("Query, we do know it's <economia> ");
-		example.execute(queryEconomia);
-
-		String[] queryCultura = {"-m", CULTURE_QUERY_PATH, "-t", "example"};
-		System.out.println("Query, we do know it's <cultura> ");
-		example.execute(queryCultura);
-
-		String[] queryDeportes = {"-m", SPORT_QUERY_PATH, "-t", "example"};
-		System.out.println("Query, we do know it's <deportes> ");
-		example.execute(queryDeportes);
-
+		final File folder = new File(DATA_FOLER);
+		TextClassifier classifier = new TextClassifier();
+		for (final File fileEntry : folder.listFiles()) {
+			if (FileHelper.isNormalFile(fileEntry)) {
+				classifier.updateDataset(fileEntry.getPath());
+			}
+		}
+		showStadistics(classifier);
+		politicQuery(classifier);
+		cultureQuery(classifier);
+		sportQuery(classifier);
 		System.out.println("END");
 	}
 
 	/**
-	 * Main method. The following parameters are recognized:
-	 * <ul>
-	 *   <li>
-	 *      <code>-m messagefile</code><br/>
-	 *      Points to the file containing the message to classify or use for
-	 *      updating the model.
-	 *   </li>
-	 *   <li>
-	 *      <code>-c classlabel</code><br/>
-	 *      The class label of the message if model is to be updated. Omit for
-	 *      classification of a message.
-	 *   </li>
-	 *   <li>
-	 *      <code>-t modelfile</code><br/>
-	 *      The file containing the model. If it doesn't exist, it will be
-	 *      created automatically.
-	 *   </li>
-	 * </ul>
+	 * Second example
+	 * <p>
+	 * Shows how to use the <code>textClassifier</code> class building a dataset from a single
+	 * <em>arff</em> file
 	 */
-	public void execute(String[] args) {
-		try {
-			// Read message file into string.
-			String messageName = Utils.getOption('m', args);
-			if (messageName.length() == 0) {
-				throw new Exception("Must provide name of message file ('-m <file>').");
-			}
-			FileReader m = new FileReader(messageName);
-			StringBuffer message = new StringBuffer();
-			int l;
-			while ((l = m.read()) != -1) {
-				message.append((char) l);
-			}
-			m.close();
-
-			// Check if class value is given.
-			String classValue = Utils.getOption('c', args);
-
-			// If model file exists, read it, otherwise create new one.
-			String modelName = Utils.getOption('t', args);
-			if (modelName.length() == 0) {
-				throw new Exception("Must provide name of model file ('-t <file>').");
-			}
-			TextClassifier messageCl;
-			try {
-				messageCl = (TextClassifier) SerializationHelper.read(modelName);
-			} catch (FileNotFoundException e) {
-				System.out.println("NEW Classifier");
-				messageCl = new TextClassifier();
-			}
-
-			// Check if there are any options left
-			Utils.checkForRemainingOptions(args);
-
-			// Process message.
-			if (classValue.length() != 0) {
-				messageCl.updateData(message.toString(), classValue);
-			} else {
-				HashMap<String, Double> r = messageCl.classifyMessage(message.toString());
-				System.out.println(r);
-			}
-
-			// Save message classifier object only if it was updated.
-			if (classValue.length() != 0) {
-				SerializationHelper.write(modelName, messageCl);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static void _main(String[] args) {
+		TextClassifier classifier = new TextClassifier(EXAMPLE_MODEL);
+		showStadistics(classifier);
+		politicQuery(classifier);
+		cultureQuery(classifier);
+		sportQuery(classifier);
+		System.out.println("END");
 	}
+
+	/**
+	 * Third example
+	 * <p>
+	 * Shows how to use the <code>textClassifier</code> class building initially an empty dataset
+	 * and filling it manually using <code>updateData(text, class)</code> method
+	 */
+	public static void __main(String[] args) {
+		TextClassifier classifier = new TextClassifier();
+		updateDataWithExampleFiles(classifier);
+		showStadistics(classifier);
+		politicQuery(classifier);
+		cultureQuery(classifier);
+		sportQuery(classifier);
+		System.out.println("END");
+	}
+
+
+	// private methods
+
+	private static void showStadistics(TextClassifier classifier) {
+		System.out.println("Att: " + classifier.getNumAttributes());
+		System.out.println("Classes: " + classifier.getNumClasses());
+		System.out.println("Instances: " + classifier.getNumInstances());
+		System.out.println("----------------------------------");
+	}
+
+	private static void politicQuery(TextClassifier classifier) {
+		HashMap<String, Double> response;
+		System.out.println("Query, we do know it's a politic text");
+		response = classifier.classifyMessage(FileHelper.readTextFile(POLITIC_QUERY));
+		System.out.println(response);
+		System.out.println(classifier.lastClassName);
+	}
+
+	private static void cultureQuery(TextClassifier classifier){
+		HashMap<String, Double> response;
+		System.out.println("Query, we do know it's a cultural text");
+		response = classifier.classifyMessage(FileHelper.readTextFile(CULTURE_QUERY));
+		System.out.println(response);
+		System.out.println(classifier.lastClassName);
+	}
+
+	private static void sportQuery(TextClassifier classifier) {
+		HashMap<String, Double> response;
+		System.out.println("Query, we do know it's a sports text");
+		response = classifier.classifyMessage(FileHelper.readTextFile(SPORT_QUERY));
+		System.out.println(response);
+		System.out.println(classifier.lastClassName);
+	}
+
+	private static void updateDataWithExampleFiles(TextClassifier classifier) {
+		// politics
+		classifier.updateData(FileHelper.readTextFile(POLITIC_TEXT_1), MsgClassificationConstants.POLITICS);
+		classifier.updateData(FileHelper.readTextFile(POLITIC_TEXT_2), MsgClassificationConstants.POLITICS);
+		classifier.updateData(FileHelper.readTextFile(POLITIC_TEXT_3), MsgClassificationConstants.POLITICS);
+
+		// culture
+		classifier.updateData(FileHelper.readTextFile(CULTURE_TEXT_1), MsgClassificationConstants.CULTURE);
+		classifier.updateData(FileHelper.readTextFile(CULTURE_TEXT_2), MsgClassificationConstants.CULTURE);
+
+		// sport
+		classifier.updateData(FileHelper.readTextFile(SPORT_TEXT_1), MsgClassificationConstants.SPORTS);
+		classifier.updateData(FileHelper.readTextFile(SPORT_TEXT_2), MsgClassificationConstants.SPORTS);
+	}
+
+
+	// constants
+
+	private static final String DATA_FOLER = "/Users/manutero/workspace/itafy/weka-data";
+
+	private static final String EXAMPLE_MODEL =
+			TextClassifierExample.class.getResource("exampleFiles/example-model.arff").getPath();
+
+	private static final String POLITIC_QUERY =
+			TextClassifierExample.class.getResource("exampleFiles/query_politica.txt").getPath();
+
+	private static final String CULTURE_QUERY =
+			TextClassifierExample.class.getResource("exampleFiles/query_cultura.txt").getPath();
+
+	private static final String SPORT_QUERY =
+			TextClassifierExample.class.getResource("exampleFiles/query_deportes.txt").getPath();
+
+	private static final String POLITIC_TEXT_1 =
+			TextClassifierExample.class.getResource("exampleFiles/politica_1.txt").getPath();
+
+	private static final String POLITIC_TEXT_2 =
+			TextClassifierExample.class.getResource("exampleFiles/politica_2.txt").getPath();
+
+	private static final String POLITIC_TEXT_3 =
+			TextClassifierExample.class.getResource("exampleFiles/politica_3.txt").getPath();
+
+	private static final String CULTURE_TEXT_1 =
+			TextClassifierExample.class.getResource("exampleFiles/cultura_1.txt").getPath();
+
+	private static final String CULTURE_TEXT_2 =
+			TextClassifierExample.class.getResource("exampleFiles/cultura_2.txt").getPath();
+
+	private static final String SPORT_TEXT_1 =
+			TextClassifierExample.class.getResource("exampleFiles/deportes_1.txt").getPath();
+
+	private static final String SPORT_TEXT_2 =
+			TextClassifierExample.class.getResource("exampleFiles/deportes_2.txt").getPath();
 
 }
