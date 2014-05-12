6f001c808dd856d88c2d6e2040af406d43659061
manutero <manutero@gmail.com>
2014-05-12 15:26:05 +0200

    Fix: fixing api => fixed the helper class ParamsHelper

--- app/utils/helpers/ParamsHelper.java
+++ app/utils/helpers/ParamsHelper.java
@@ -2,6 +2,10 @@ package utils.helpers;
 
 import java.util.HashMap;
 import java.util.Map;
+import models.categories.Category;
+import models.geoLocation.Area;
+import models.geoLocation.AvaibleLocations;
+import twitter4j.GeoLocation;
 
 
 /**
@@ -24,7 +28,6 @@ public class ParamsHelper {
 		}
 	}
 
-	@SuppressWarnings("unused")
 	private String[] removeDuplicates(String[] values) {
 		//TODO
 		return values;
@@ -34,13 +37,49 @@ public class ParamsHelper {
 		return params.isEmpty();
 	}
 
-	public String get(String key) {
-		System.out.println(key + "  =>  " + params.get(key));
-		return params.get(key);
-	}
-
 	public boolean has(String key) {
 		return params.get(key) != null;
+	}
+
+	public Area defineArea() {
+		Area area = null;
+		if (has("area")) {
+			AvaibleLocations trustedLocation = AvaibleLocations.asLocation(params.get("area"));
+			area = Area.createLocation(trustedLocation);
+		} else if ((has("latitude")) && (has("longitude")) && has("radio")) {
+			double latitude = Double.valueOf(params.get("latitude")).doubleValue();
+			double longitude = Double.valueOf(params.get("longitude")).doubleValue();
+			double radio = Double.valueOf(params.get("radio")).doubleValue();
+			GeoLocation coordenates = new GeoLocation(latitude, longitude);
+			area = Area.createLocation(coordenates, radio);
+		}
+		return area;
+	}
+
+	// FIXME return Enum instead of String
+	public String defineGenre() {
+		String genre = null;
+		if (has("genre")) {
+			genre = params.get("genre");
+		}
+		return genre;
+	}
+
+	public String defineUser() {
+		String userName = null;
+		if (has("user")) {
+			userName = params.get("user");
+		}
+		return userName;
+	}
+
+	// XXX check this
+	public Category defineCategory() {
+		Category category = null;
+		if (has("category")) {
+			Category.createCategory(params.get("category"));
+		}
+		return category;
 	}
 
 }
